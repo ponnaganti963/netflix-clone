8c4fc7d7208b766c1a489d54131308b6863c6dbb
ponnaganti963 <ponnagantimanikanta26@gmail.com>
2022-07-02 21:11:14 +0530

    new features

---
+++ src/nav.js
@@ -0,0 +1,28 @@
+import React, {useState,useEffect}  from "react";
+import "./nav.css";
+
+function Nav(){
+  const [show, handleShow] = useState(false);
+
+ useEffect(() =>{
+   window.addEventListener("scroll", ()=>{
+     if(window.scrollY > 100){
+        handleShow(true);
+     }else handleShow(false);
+   });
+   return () => {
+     window.removeEventListener("scroll");
+   };
+ },[]);
+
+  return(
+     <div className={`nav ${show && "nav__black"}`}>
+         <img
+         className="nav__img"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="netflix Logo"/>
+     </div>
+  )
+}
+
+export default Nav;
